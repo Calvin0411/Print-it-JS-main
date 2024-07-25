d39856884d49997ab12e66ba52a7586a3233be68
Calvin0411 <ramjeeawoncalvin@gmail.com>
2024-07-25 11:03:41 +0200

    Mise à jour de mon script
    J'ai revus mon script afin de comprendre mon fonctionnement

--- assets/script.js
+++ assets/script.js
@@ -19,44 +19,57 @@ const slides = [
 	}
 ];
 
-console.log("Hello World !");
-	// premier pas sur le Java Script
+console.log("Hello World!");
 
+//déclaration de mes variables d'éléments classes
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const bannerImg = document.querySelector(".banner-img");
-const bannerText = document.querySelector("#banner p");
+const bannerText = document.querySelector("p");
 const dots = document.querySelectorAll(".dot");
 
-let currentIndex = 0;
+let slidePrincipale = 0; 
 
-function onArrowLeftClick() {
-	currentIndex= (currentIndex > 0)? currentIndex - 1 :slides.length -1;
+function carrousel() {
+    // Met à jour la source de l'image
+    bannerImg.src = './assets/images/slideshow/' + slides[slidePrincipale].image;
+    
+    // Met à jour la description
+    bannerText.innerHTML = slides[slidePrincipale].tagLine;
+
+    // Met à jour les points de navigation
+    dots.forEach((dot, index) => {
+        // Vérifie si le dot est sélectionné
+        if (index === slidePrincipale) {
+            // Si le dot est bon, elle ajoute la classe
+            dot.classList.add("dot_selected");
+        } else {
+            // Sinon,  elle retire la classe
+            dot.classList.remove("dot_selected");
+        }
+    });
 }
 
-
-function onArrowRightClick() {
-	currentIndex= (currentIndex > 0)? currentIndex - 1 :slides.length -1;
-}
-
-
-function updateSlide(index) {
-	const slide = slides[index];
-	bannerImg.src = `./assets/images/slideshow/${slide.image}`;
-	bannerText.innerHTML = slide.tagLine;
-}
-
-
+//écouteurs d'événements pour mes flèches
 arrowLeft.addEventListener("click", function() {
-	currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
-	updateSlide(currentIndex);
-	console.log("Click sur la flèche gauche");
+		console.log ("j'ai cliqué sur le bouton gauche")
+
+    if (slidePrincipale === 0) {
+        slidePrincipale = slides.length - 1; // Passe à la dernière diapositive si elle est à la première
+    } else {
+        slidePrincipale--; // sinon elle passe à la diapositive précédente
+    }
+    carrousel(); // Met à jour l carrousel 
 });
 
 arrowRight.addEventListener("click", function() {
-	currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
-	updateSlide(currentIndex);
-	console.log("Click sur la flèche droite");
+		console.log ("j'ai cliqué sur le bouton droit")
+    if (slidePrincipale === slides.length - 1) {
+        slidePrincipale = 0; // Revenient à la première diapositive si elle est à la dernière
+    } else {
+        slidePrincipale++; // Passe à la diapositive suivante
+    }
+    carrousel();
 });
 
 
